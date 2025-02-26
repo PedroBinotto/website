d5a7629d4e64e8f53c966a2c228f6be63881e74f
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-02-26 16:54:27 -0300

    fix navbar

--- static/js/mobile-navbar.js
+++
@@ -1,4 +0,0 @@
-function toggleNavbar() {
-  document.getElementById('menu-icon').classList.toggle('rotate-90');
-  document.getElementById('mobile-menu').classList.toggle('hidden');
-}
--- static/js/utils.js
+++ static/js/utils.js
@@ -12,3 +12,8 @@ function toggleDarkMode() {
     localStorage.theme = 'dark';
   }
 }
+
+function toggleNavbar() {
+  document.getElementById('menu-icon').classList.toggle('rotate-90');
+  document.getElementById('mobile-menu').classList.toggle('hidden');
+}
